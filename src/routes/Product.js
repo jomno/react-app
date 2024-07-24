06240b9d794999a87fe15b3595a9f764b36a5517
hyoni <jih70004@gmail.com>
2024-07-24 15:50:44 +0900

    카드 삭제 기능 추가

--- src/routes/Product.js
+++ src/routes/Product.js
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 
 function ProductCardList({ products }) {
+  const deletBtn = (e) => {
+    e.preventDefault();
+    const btnParent = e.target.parentElement
+    btnParent.parentElement.remove();
+  }
+
+  const product = products.map(product => (
+    <li className="item" key={product.id}>
+      <div className="flex-wrap">
+        <span className="item-rating">평점: {product.rating.rate}</span>
+        <figure className="item-image">
+          <img src={product.image} alt={product.title} />
+        </figure>
+      </div>
+      <h3 className="item-title">{product.title}</h3>
+      <div className="item-description">{product.description}</div>
+      <div className="item-price">가격: ₩{(product.price * 1200).toLocaleString()}</div>
+      <div className="item-delete">
+        <button className="delete-button" onClick={deletBtn}>삭제</button>
+      </div>
+    </li>
+  ))
+
   return (
     <ul className="product-list">
-      {products.map(product => (
-        <li className="item" key={product.id}>
-          <div className="flex-wrap">
-            <span className="item-rating">평점: {product.rating.rate}</span>
-            <figure className="item-image">
-              <img src={product.image} alt={product.title} />
-            </figure>
-          </div>
-          <h3 className="item-title">{product.title}</h3>
-          <div className="item-description">{product.description}</div>
-          <div className="item-price">가격: ₩{(product.price * 1200).toLocaleString()}</div>
-          <div className="item-delete">
-            <button className="delete-button">삭제</button>
-          </div>
-        </li>
-      ))}
+      {product}
     </ul>
   )
 }
